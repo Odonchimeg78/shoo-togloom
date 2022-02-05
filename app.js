d9bba9ee9c3e923f393b1b3e28d6de06fde75b9b
Odonchimeg78 <besudodonchimeg78@gmail.com>
2022-02-05 16:39:03 +0900

    togloomiin nukhtsul hadgalah

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ function initGame() {
  activePlayer = 0 ;
 
 // тоглогчдийн  цуглуулсан оноог хадгалах 
- scores = [0,0];
+ scores = [0 , 0];
 
 // тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах 
  roundScore = 0;
@@ -62,7 +62,7 @@ document.getElementById('current-1').textContent = "0";
 
 // шоог шидэх эвент листенер btn-roll
 document.querySelector('.btn-roll').addEventListener("click", function () {
-                  
+                         
 if (isNewGame) {
                 
  // 1 - 6 хүртэл утгыг санамсаргүйгээр гаргаж авна.
@@ -78,48 +78,54 @@ if (isNewGame) {
                   
   //Буусан тоо 1-ээс ялгаатай бол идвэхтэй Тоглогчийн ээлжийн оноог нэмэгдүүднэ.
 
-                                if (diceNumber !== 1) {
+                 if (diceNumber !== 1) {
                   //1-ээс ялгаатай тоо буулаа. Буусан тоог тоглогчид нэмж өгнө.
-                                    roundScore = roundScore + diceNumber;
-                                document.getElementById('current-' + activePlayer).textContent = roundScore;
-                                                
-
-                                if(scores[activePlayer] >=20){
-                                 // тоглоомыг дууссан төлөвт оруулна.
-                                isNewGame = false;
-                                 //Ялагч гэсэн техт нэрнийх нь оронд гаргана.
+                roundScore = (roundScore + diceNumber);
+                             
+                                 
+                document.getElementById("current-" + activePlayer).textContent = roundScore;
+                           
+                        // Уг тоглогчийн цуглуулсан Ээлжийн оноог глобаль оноон дээр нэмж өгнө.
+                           scores[activePlayer] = ( roundScore +scores[activePlayer]);                
+                       
+                     // Дэлгэц дээр оноог өөрчилнө.          
+document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];                       
+                 //   тоглогч хожсон эсхийг  шалгах(оноо нь 100-с их үү?)               
+                 if (scores[activePlayer] >= 100) {
+                       
+                 
+                // тоглоомыг дууссан төлөвт оруулна.
+                                 isNewGame=false;                                                                   
+                // Ялагч гэсэн техт нэрнийх нь оронд гаргана.
                                 document.getElementById("name-" + activePlayer).textContent = "WINNER!!!!";
                                 document.querySelector(".player-" + activePlayer + "-panel").classList.add('winner');
-                                                }else {
-                                                isNewGame = true; 
-                                                document.querySelector('.btn-roll').addEventListener("click");
+                                }else {
+                                isNewGame = true; 
+                                //   document.querySelector('.btn-roll').addEventListener("click"); 
                                   }
                                 } else {
-                                                switchToNextPlayer(); 
+                                 switchToNextPlayer(); 
+                                
                                 } 
                 } else { alert(' Togloom duuslaa New game button push ');
- }              
+                           }              
 });
-                
-                           
-
 
 // HOLD товчны эвент лестенер
 document.querySelector(' .btn-hold ').addEventListener("click", function () {
-
-                if (isNewGame) {
-                        // Уг тоглогчийн цуглуулсан Ээлжийн оноог глобаль оноон дээр нэмж өгнө.
-               scores[activePlayer] = scores[activePlayer] + roundScore;
+                //     Уг тоглогчийн цуглуулсан Ээлжийн оноог глобаль оноон дээр нэмж өгнө..
+                  scores[activePlayer] = (scores[activePlayer] + roundScore);
                        
-                // Дэлгэц дээр оноог өөрчилнө.
-                   document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
-                   
-                   // Уг тоглогч хожсон эсхийг  шалгах(оноо нь 100-с их үү?)
-                if (scores[activePlayer] >=20) {
-                                // тоглоомыг дууссан төлөвт оруулна.
+                   // Дэлгэц дээр оноог өөрчилнө.
+                document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+                if (isNewGame) {
+                 // Уг тоглогч хожсон эсхийг  шалгах(оноо нь 100-с их үү?)
+                       if (scores[activePlayer] >= 100){ 
+                 
+                   // тоглоомыг дууссан төлөвт оруулна.
                                 isNewGame = false;
-                //            Ялагч гэсэн техт нэрнийх нь оронд гаргана.
-                                document.getElementById("name-" + activePlayer).textContent = "WINNER!!!!";
+                //Ялагч гэсэн техт нэрнийх нь оронд гаргана.
+                                document.getElementById("name-" + activePlayer).textContent = "WINNER!!";
                                 document.querySelector(".player-" + activePlayer + "-panel").classList.add('winner');
                 } else {
 
@@ -136,12 +142,7 @@ document.querySelector(' .btn-hold ').addEventListener("click", function () {
 //  энэ функц нь тоглох ээлжийг дараачийн тоглогчид шилжүүлдэг.
 // DRY Don't repeat yourself!!!!!!!!
 function switchToNextPlayer() {
-                // Уг тоглогчийн цуглуулсан Ээлжийн оноог глобаль оноон дээр нэмж өгнө.
-
-               scores[activePlayer] = scores[activePlayer] + roundScore;
-
-                // Дэлгэц дээр оноог нь өөрчилнө.
-                document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+                
                   
                   
 
@@ -152,13 +153,9 @@ function switchToNextPlayer() {
                                  //   тоглогчийн ээлжийг нөгөө тоглогчруу шилжүүлнэ
                                     // хэрэв идвэхтэй тоглогч 0 байвал идвэхтэй тоглогчийг 1 болго.
                                     // Үгүй бол хэрэв идвэхтэй тоглогчийг 0 болго.
+                                    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
                                    
-                                    // if (activePlayer === 0) {
-                                    //                   activePlayer = 1;
-                                    // } else {
-                                    //                   activePlayer = 0
-                                    // }
-                                  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+                                    
                                 
                                     // идвэхтэй тоглогчийн тэмдэг цэг шилжүүлэх
                                   document.querySelector('.player-0-panel ').classList.toggle("active");
